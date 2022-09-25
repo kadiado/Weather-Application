4f8d24a2868ab45d2ad1238ac0fcf7e72edf5e4c
Marco Thulio <marcothulio2@gmail.com>
2022-09-25 11:58:47 -0300

    add

---
+++ js/weather.js
@@ -0,0 +1,50 @@
+const APIkey = 'vGxjKJBKAI2GGojkHhE3DOxvADNq8Ud5'
+
+const baseUrl = 'http://dataservice.accuweather.com/'
+
+const getCityUrl = cityName => 
+  `${baseUrl}locations/v1/cities/search?apikey=${APIkey}&q=${cityName}`
+
+const getWeatherUrl = (cityKey) => `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIkey}&language=pt-br`
+
+  
+const FetchData = async url => {
+ try{
+    const response = await fetch(url)
+
+    if(!response.ok) {
+       throw new Error('Não foi possivel obter os dados');
+    }
+
+    return response.json()
+    
+  } catch ({name, message}) { //destruct propriedades catch
+    alert(`${name} ${message}`)
+  }
+}
+
+const getCityData = cityName => FetchData(getCityUrl(cityName))
+
+const getCityWeather = cityKey => FetchData(getWeatherUrl(cityKey))
+
+
+// getCityWeather('São Paulo')
+//   .then(console.log)
+
+
+/* const getCityWeather = async cityName => {  
+  try {
+    
+    if(!response.ok) {
+      throw new Error('Não foi possivel obter os dados')
+    }
+    
+    const [cityWeatherData] = await response.json()
+    console.log(cityWeatherData);
+    return cityWeatherData
+
+  } catch ({name, message}) {
+    alert(`${name} ${message}`)
+  }
+}  */
+
